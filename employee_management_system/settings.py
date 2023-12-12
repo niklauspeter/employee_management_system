7efa784b7fcdfb9c57836ada94d2ac3d317b4ab3
klaus <oriokiklaus@gmail.com>
2023-12-13 01:00:44 +0300

    add admin page template and relevant static and media files

--- employee_management_system/settings.py
+++ employee_management_system/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +27,12 @@ SECRET_KEY = 'django-insecure-cndbl$ejdx4+d-!c8f=#_@11e*2)xxyx_3kd*lm$e_r1e0w^77
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Application definition
--- employee_management_system/urls.py
+++ employee_management_system/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
 
+from django.urls import path
+from employee_management_system import settings
+from employee_management_app import views
+
 urlpatterns = [
+    path('demo', views.showDemoPage),
     path('admin/', admin.site.urls),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
