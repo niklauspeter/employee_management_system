269a270dfd0e702a07290edaa119aa2abde29178
klaus <oriokiklaus@gmail.com>
2023-12-12 23:41:44 +0300

    create sql database and configure settings.py to include employee_management database

--- employee_management_system/settings.py
+++ employee_management_system/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'employee_management_app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,14 @@ WSGI_APPLICATION = 'employee_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'employee_management_system',
+        'USER':'employee_management_system',
+        'PASSWORD': 'employee_management_password',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
