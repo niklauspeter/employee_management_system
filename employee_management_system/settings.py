bce4be6db93ad18ddc9325b2f29001f9d04b30ae
klaus <oriokiklaus@gmail.com>
2023-12-15 00:43:32 +0300

    add custom user model, to customise login experience depending on user type, tweak admin, staff, and employee models respectively

--- employee_management_system/settings.py
+++ employee_management_system/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+AUTH_USER_MODEL = 'employee_management_app'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
