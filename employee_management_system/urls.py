7d3189348d7c4d2d286c70dc50a177e21699c1ee
klaus <oriokiklaus@gmail.com>
2023-12-18 22:26:14 +0300

    add path to forms for adding and saving employees

--- employee_management_system/urls.py
+++ employee_management_system/urls.py
@@ -31,5 +31,9 @@ urlpatterns = [
     path('logout_user', views.logout_user,name="logout"),
     path('admin_home',HodViews.admin_home,name="admin_home"),
     path('add_staff',HodViews.add_staff,name="add_staff"),
-    path('add_staff_save',HodViews.add_staff_save,name="add_staff_save")
+    path('add_staff_save',HodViews.add_staff_save,name="add_staff_save"),
+    path('add_course', HodViews.add_course,name="add_course"),
+    path('add_course_save', HodViews.add_course_save,name="add_course_save"),
+    path('add_student', HodViews.add_student,name="add_student"),
+    path('add_student_save', HodViews.add_student_save,name="add_student_save")
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
