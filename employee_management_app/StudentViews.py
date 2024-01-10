2fda0ae6d45990571ec735fd17e8f3659edf0103
klaus <oriokiklaus@gmail.com>
2024-01-10 09:47:26 +0300

    add view function for students to view attendance html page, add corresponding url pathh

--- employee_management_app/StudentViews.py
+++ employee_management_app/StudentViews.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+import datetime
+
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
+from employee_management_app.models import Students, Courses, Subjects, CustomUser, Attendance, AttendanceReport, \
+    LeaveReportStudent, FeedBackStudent, NotificationStudent, StudentResult, OnlineClassRoom, SessionYearModel
 
 
 def student_home(request):
     return render(request,"student_template/student_home_template.html")
+
+def student_view_attendance(request):
+    student=Students.objects.get(admin=request.user.id)
+    course=student.course_id
+    subjects=Subjects.objects.filter(course_id=course)
+    return render(request,"student_template/student_view_attendance.html",{"subjects":subjects})
