ebf94267cfb1396a565827c6432e6e35a0a74ccf
klaus <oriokiklaus@gmail.com>
2024-01-22 16:41:46 +0300

    make changes to variables in employee views.py file to reflect changes in models.py file

--- employee_management_app/EmployeeViews.py
+++ employee_management_app/EmployeeViews.py
@@ -6,65 +6,76 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
-from employee_management_app.models import Students, Courses, Subjects, CustomUser, Attendance, AttendanceReport, \
-    LeaveReportStudent, FeedBackStudent, NotificationStudent, StudentResult, OnlineClassRoom, SessionYearModel
+from employee_management_app.models import Employees, Departments, Roles, CustomUser, Attendance, AttendanceReport, \
+    LeaveReportEmployee, FeedbackEmployee, NotificationEmployee, EmployeeResult, OnlineClassRoom, SessionYearModel
 
 
-def student_home(request):
-    student_final_result = []
-    student_mid_result = []
+def employee_home(request):
+    employee_final_result = []
+    employee_mid_result = []
 
-    student_obj=Students.objects.get(admin=request.user.id)
-    studentresult=StudentResult.objects.filter(student_id=student_obj.id)
-    attendance_total=AttendanceReport.objects.filter(student_id=student_obj).count()
-    attendance_present=AttendanceReport.objects.filter(student_id=student_obj,status=True).count()
-    attendance_absent=AttendanceReport.objects.filter(student_id=student_obj,status=False).count()
-    course=Courses.objects.get(id=student_obj.course_id.id)
-    subjects=Subjects.objects.filter(course_id=course).count()
-    subjects_data=Subjects.objects.filter(course_id=course)
-    session_obj=SessionYearModel.object.get(id=student_obj.session_year_id.id)
-    class_room=OnlineClassRoom.objects.filter(subject__in=subjects_data,is_active=True,session_years=session_obj)
+    employee_obj = Employees.objects.get(admin=request.user.id)
+    employeeresult = EmployeeResult.objects.filter(employee_id=employee_obj.id)
+    attendance_total = AttendanceReport.objects.filter(employee_id=employee_obj).count()
+    attendance_present = AttendanceReport.objects.filter(employee_id=employee_obj, status=True).count()
+    attendance_absent = AttendanceReport.objects.filter(employee_id=employee_obj, status=False).count()
+    department = Departments.objects.get(id=employee_obj.department_id.id)
+    roles = Roles.objects.filter(department_id=department).count()
+    roles_data = Roles.objects.filter(department_id=department)
+    session_obj = SessionYearModel.object.get(id=employee_obj.session_year_id.id)
+    class_room = OnlineClassRoom.objects.filter(role__in=roles_data, is_active=True, session_years=session_obj)
 
-    for studi in studentresult:
-        student_final_result.append(int(studi.subject_exam_marks))
-        student_mid_result.append(int(studi.subject_assignment_marks))
+    for employee_result in employeeresult:
+        employee_final_result.append(int(employee_result.exam_marks))
+        employee_mid_result.append(int(employee_result.assignment_marks))
 
-    subject_name=[]
-    data_present=[]
-    data_absent=[]
-    subject_data=Subjects.objects.filter(course_id=student_obj.course_id)
-    for subject in subject_data:
-        attendance=Attendance.objects.filter(subject_id=subject.id)
-        attendance_present_count=AttendanceReport.objects.filter(attendance_id__in=attendance,status=True,student_id=student_obj.id).count()
-        attendance_absent_count=AttendanceReport.objects.filter(attendance_id__in=attendance,status=False,student_id=student_obj.id).count()
-        subject_name.append(subject.subject_name)
+    role_name = []
+    data_present = []
+    data_absent = []
+    role_data = Roles.objects.filter(department_id=employee_obj.department_id)
+    for role in role_data:
+        attendance = Attendance.objects.filter(role_id=role.id)
+        attendance_present_count = AttendanceReport.objects.filter(attendance_id__in=attendance, status=True,
+                                                                   employee_id=employee_obj.id).count()
+        attendance_absent_count = AttendanceReport.objects.filter(attendance_id__in=attendance, status=False,
+                                                                  employee_id=employee_obj.id).count()
+        role_name.append(role.role_name)
         data_present.append(attendance_present_count)
         data_absent.append(attendance_absent_count)
 
-    return render(request,"student_template/student_home_template.html",{"total_attendance":attendance_total,"attendance_absent":attendance_absent,"attendance_present":attendance_present,"subjects":subjects,"data_name":subject_name,"data1":data_present,"data2":data_absent,"class_room":class_room,"data3":student_mid_result,"data4":student_final_result})
+    return render(request, "employee_template/employee_home_template.html",
+                  {"total_attendance": attendance_total, "attendance_absent": attendance_absent,
+                   "attendance_present": attendance_present, "roles": roles, "data_name": role_name,
+                   "data1": data_present, "data2": data_absent, "class_room": class_room,
+                   "data3": employee_mid_result, "data4": employee_final_result})
 
-def student_view_attendance(request):
-    student=Students.objects.get(admin=request.user.id)
-    course=student.course_id
-    subjects=Subjects.objects.filter(course_id=course)
-    return render(request,"student_template/student_view_attendance.html",{"subjects":subjects})
 
-def student_view_attendance_post(request):
-    subject_id=request.POST.get("subject")
-    start_date=request.POST.get("start_date")
-    end_date=request.POST.get("end_date")
+def employee_view_attendance(request):
+    employee = Employees.objects.get(admin=request.user.id)
+    department = employee.department_id
+    roles = Roles.objects.filter(department_id=department)
+    return render(request, "employee_template/employee_view_attendance.html", {"roles": roles})
 
-    start_data_parse=datetime.datetime.strptime(start_date,"%Y-%m-%d").date()
-    end_data_parse=datetime.datetime.strptime(end_date,"%Y-%m-%d").date()
-    subject_obj=Subjects.objects.get(id=subject_id)
-    user_object=CustomUser.objects.get(id=request.user.id)
-    stud_obj=Students.objects.get(admin=user_object)
 
-    attendance=Attendance.objects.filter(attendance_date__range=(start_data_parse,end_data_parse),subject_id=subject_obj)
-    attendance_reports=AttendanceReport.objects.filter(attendance_id__in=attendance,student_id=stud_obj)
-    return render(request,"student_template/student_attendance_data.html",{"attendance_reports":attendance_reports})
+def employee_view_attendance_post(request):
+    role_id = request.POST.get("role")
+    start_date = request.POST.get("start_date")
+    end_date = request.POST.get("end_date")
 
-def student_view_result(request):
-    student=Students.objects.get(admin=request.user.id)
-    studentresult=StudentResult.objects.filter(student_id=student.id)
-    return render(request,"student_template/student_result.html",{"studentresult":studentresult})
+    start_date_parse = datetime.datetime.strptime(start_date, "%Y-%m-%d").date()
+    end_date_parse = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
+    role_obj = Roles.objects.get(id=role_id)
+    user_object = CustomUser.objects.get(id=request.user.id)
+    emp_obj = Employees.objects.get(admin=user_object)
+
+    attendance = Attendance.objects.filter(attendance_date__range=(start_date_parse, end_date_parse),
+                                           role_id=role_obj)
+    attendance_reports = AttendanceReport.objects.filter(attendance_id__in=attendance, employee_id=emp_obj)
+    return render(request, "employee_template/employee_attendance_data.html",
+                  {"attendance_reports": attendance_reports})
+
+
+def employee_view_result(request):
+    employee = Employees.objects.get(admin=request.user.id)
+    employeeresult = EmployeeResult.objects.filter(employee_id=employee.id)
+    return render(request, "employee_template/employee_result.html", {"employeeresult": employeeresult})
